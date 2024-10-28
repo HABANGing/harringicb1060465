b121b5122d9cad0dfe11c6a9b00f1eb8abfa0cba
vink-wedding <vink_vp@naver.com>
2024-10-28 16:35:23 +0900

    first commit

--- skin.js
+++ skin.js
@@ -1,7 +1,7 @@
 // Garden Gnome Software - Skin
 // Pano2VR 7.1.2/20913
 // Filename: ?? ?? ?? ????.ggsk
-// Generated 2024-10-28T16:02:43
+// Generated 2024-10-28T16:34:44
 
 function pano2vrSkin(player,base) {
 	player.addVariable('vis_floorplan', 2, false, { ignoreInState: 0  });
